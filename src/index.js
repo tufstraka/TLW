6c47d8c7d314bc68e616e78a608a7b0937361ee3
kadimakeith <keithkadima@gmail.com>
2023-04-24 19:52:09 +0300

    add analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
