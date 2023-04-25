72d61a6ec7751203919e52d2df30842121b05901
kadimakeith <keithkadima@gmail.com>
2023-04-25 16:05:43 +0300

    Create Transaction.js

---
+++ src/components/Transaction.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Web3 from 'web3';
+
+function Transaction( status, sender, recipient ) {
+  return (
+    <div>
+      <p>{sender}</p>
+      <p>{recipient}</p>
+      <p>{status}</p>
+      {status.startsWith('Transaction') && (
+        <p>
+          View transaction on Etherscan:{' '}
+          <a href={`https://etherscan.io/tx/${status.split(': ')[1]}`} target="_blank" rel="noopener noreferrer">{status.split(': ')[1]}</a>>
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default Transaction;
